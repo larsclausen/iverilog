460bdab9350a1826fd158d042bbd07bb555a5331
Lars-Peter Clausen <lars@metafoo.de>
2021-12-28 16:44:58 +0100

    cmpi/addi/subi

--- tgt-vvp/vvp_process.c
+++ tgt-vvp/vvp_process.c
@@ -1620,7 +1620,7 @@ static int show_stmt_repeat(ivl_statement_t net, ivl_scope_t sscope)
       int rc = 0;
       unsigned lab_top = local_count++, lab_out = local_count++;
       ivl_expr_t expr = ivl_stmt_cond_expr(net);
-      const char *sign = ivl_expr_signed(expr) ? "s" : "u";
+      const char *sign = ivl_expr_signed(expr) ? "s" : "e";
 
       show_stmt_file_line(net, "Repeat statement.");
 
@@ -1631,14 +1631,12 @@ static int show_stmt_repeat(ivl_statement_t net, ivl_scope_t sscope)
 	   unsigned, then we only need to try to escape if expr==0 as
 	   it will never be <0. */
       fprintf(vvp_out, "T_%u.%u %%dup/vec4;\n", thread_count, lab_top);
-      fprintf(vvp_out, "    %%pushi/vec4 0, 0, %u;\n", ivl_expr_width(expr));
-      fprintf(vvp_out, "    %%cmp/%s;\n", sign);
+      fprintf(vvp_out, "    %%cmpi/%s 0, 0, %u;\n", sign, ivl_expr_width(expr));
       if (ivl_expr_signed(expr))
 	    fprintf(vvp_out, "    %%jmp/1xz T_%u.%u, 5;\n", thread_count, lab_out);
       fprintf(vvp_out, "    %%jmp/1 T_%u.%u, 4;\n", thread_count, lab_out);
 	/* This adds -1 (all ones in 2's complement) to the count. */
-      fprintf(vvp_out, "    %%pushi/vec4 1, 0, %u;\n", ivl_expr_width(expr));
-      fprintf(vvp_out, "    %%sub;\n");
+      fprintf(vvp_out, "    %%subi 1, 0 %u;\n", ivl_expr_width(expr));
 
       rc += show_statement(ivl_stmt_sub_stmt(net), sscope);
 
