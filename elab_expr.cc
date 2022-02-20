98bdd6f707f1081ab3986868a80ba72fa51cd457
Lars-Peter Clausen <lars@metafoo.de>
2022-02-20 12:27:07 +0100

    Elaborate unpacked array concatenation values with the correct context
    Values in a unpacked array concatenation are evaluated in a self-determined
    context, but the result must then be assignment cast to element type of the
    array.
    
    This is very similar how values in a non-array concatenation are evaluated.
    
    E.g. the following two are equivalent
    
    ```
    int unsigned d[]= {a, b};
    ```
    
    ```
    int unsigned d[] = new [2];
    d[0] = {a};
    d[1] = {b};
    ```
    
    and should give the same results.
    
    But there are some important differences.
    
    1) Non-array concatenation only works on vectorable expressions, while
       array concatenation works on any expression with a self-determined
       type.
    2) Non-array concatenation will evaluate the expression always as unsigned.
       While for array expression the signedness of the expression is preserved.
       This means that signed values will be sign extended if their width is
       smaller than the array element type.
    
    This is different from array pattern assignment where the expressions are
    evaluated in a context of the array element type.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- elab_expr.cc
+++ elab_expr.cc
@@ -3626,6 +3626,8 @@ static int concat_depth = 0;
 NetExpr* PEConcat::elaborate_expr(Design*des, NetScope*scope,
 				  ivl_type_t ntype, unsigned flags) const
 {
+      bool need_const = NEED_CONST & flags;
+
       switch (ntype->base_type()) {
 	  case IVL_VT_QUEUE:
 // FIXME: Does a DARRAY support a zero size?
@@ -3644,7 +3646,10 @@ NetExpr* PEConcat::elaborate_expr(Design*des, NetScope*scope,
 		  ivl_type_t elem_type = array_type->element_type();
 		  vector<NetExpr*> elem_exprs (parms_.size());
 		  for (size_t idx = 0 ; idx < parms_.size() ; idx += 1) {
-			NetExpr*tmp = parms_[idx]->elaborate_expr(des, scope, elem_type, flags);
+			NetExpr*tmp = elab_and_eval(des, scope, parms_[idx],
+						    -1, need_const);
+			tmp = assignment_cast(des, tmp, elem_type->base_type(),
+					      elem_type->packed_width());
 			elem_exprs[idx] = tmp;
 		  }
 
