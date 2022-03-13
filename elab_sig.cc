d3b4929a432f1538bf41188caf70c85a4daa3f9f
Lars-Peter Clausen <lars@metafoo.de>
2022-03-13 09:50:52 +0100

    Prevent invalid port redeclaration
    (System)Verilog allows to declare the port direction separate from the
    signal declaration. E.g.
    
    ```
    output x;
    integer x;
    ```
    
    But this is only allowed if the port declaration
     * does not have an explicit net type
     * does not have an explicit data type
     * is a non-ANSI style declaration
    
    For all other cases of port declarations the signal is considered fully
    defined and it is not allowed to have a separate signal declaration.
    
    In addition if the declared packed dimensions need to match between the
    port and signal declaration.
    
    In the current implementation there are a few cases where this is not
    handled correctly.
    
    1) It is possible to declare non-ANSI task ports with the same name over
    and over again, if it was declared a signal before the port.
    
    ```
    task t;
      string x;
      input logic x;
      output real x;
    endtask
    ```
    
    2) It is possible to re-declare non-ANSI input ports of a module that have
    a data type, but no explicit net type.
    
    ```
    module M;
      input integer x;
      wire integer x;
    endmodule
    ```
    
    3) It is possible to re-declare ANSI ports if it has an implicit data type.
    
    ```
    module M(output [1:0] x);
      reg [1:0] x;
    endmodule
    ```
    
    4) It is possible to declare a vector signal for a scalar non-ANSI task
    port.
    
    ```
    task t;
    input x;
    reg [7:0] x;
    ```
    
    To handle all of these correctly refactor signal declaration and lookup a
    bit.
    
    The PWire class that represents a signal already has two flags `port_set_`
    and `net_set_`. These flags indicate whether a signal has already been used
    in a port or signal declaration. A port declaration that includes an
    explicit data type is considered both a port and signal declaration.
    
    Use these flags to decide whether it is possible to extend an existing
    declaration. E.g. when creating a port without an explicit data type and a
    PWire by that name already exists and the `port_set_` flag is not set
    extend the existing PWire. On the other hand if the `port_set_` flag is
    already set report an error.
    
    Similar for signals but with the `net_set_` flag.
    
    For port declarations with an explicit data type or ANSI style port
    declarations it is always an error if a PWire by that name already exists.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- elab_sig.cc
+++ elab_sig.cc
@@ -1038,8 +1038,6 @@ NetNet* PWire::elaborate_sig(Design*des, NetScope*scope) const
 			     << "Evaluate ranges for port " << basename() << endl;
 		  }
 		  dimensions_ok &= evaluate_ranges(des, scope, this, plist, port_);
-		  nlist = plist;
-		    /* An implicit port can have a range so note that here. */
 	    }
             assert(port_set_ || port_.empty());
 
@@ -1060,9 +1058,10 @@ NetNet* PWire::elaborate_sig(Design*des, NetScope*scope) const
 		       << ". Now check for consistency." << endl;
 	    }
 
-	    /* We have a port size error */
-            if (port_set_ && net_set_ && !test_ranges_eeq(plist, nlist)) {
-
+	    /* We have a port size error. Skip this if the dimensions could not
+	     * be evaluated since it will likely print non-sensical errors. */
+            if (port_set_ && net_set_ && !test_ranges_eeq(plist, nlist) &&
+	        dimensions_ok) {
 		  /* Scalar port with a vector net/etc. definition */
 		  if (port_.empty()) {
 			if (!gn_io_range_error_flag) {
@@ -1101,7 +1100,7 @@ NetNet* PWire::elaborate_sig(Design*des, NetScope*scope) const
 		  }
             }
 
-	    packed_dimensions = nlist;
+	    packed_dimensions = net_set_ ? nlist : plist;
 	    wid = netrange_width(packed_dimensions);
 	    if (wid > warn_dimension_size) {
 		  cerr << get_fileline() << ": warning: Vector size "
@@ -1113,17 +1112,6 @@ NetNet* PWire::elaborate_sig(Design*des, NetScope*scope) const
       unsigned nattrib = 0;
       attrib_list_t*attrib_list = evaluate_attributes(attributes, nattrib,
 						      des, scope);
-
-      if (data_type_ == IVL_VT_REAL && !packed_dimensions.empty()) {
-	    cerr << get_fileline() << ": error: real ";
-	    if (wtype == NetNet::REG) cerr << "variable";
-	    else cerr << "net";
-	    cerr << " '" << name_
-	         << "' cannot be declared as a vector, found a range "
-		 << packed_dimensions << "." << endl;
-	    des->errors += 1;
-	    return 0;
-      }
 
 	/* If the net type is supply0 or supply1, replace it
 	   with a simple wire with a pulldown/pullup with supply
