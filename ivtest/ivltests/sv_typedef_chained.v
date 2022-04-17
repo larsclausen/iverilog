4881ab8d1216333120cb2ddd4e164c9c0cc7593d
Lars-Peter Clausen <lars@metafoo.de>
2022-04-17 14:00:21 +0200

    Add regression test for chained type definitions
    Check that chained type definitions declared in different scopes are
    handled correctly.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

---
+++ ivtest/ivltests/sv_typedef_chained.v
@@ -0,0 +1,43 @@
+// Check that chained type definitions declared in different scopes work
+// correctly.
+
+package P1;
+  localparam A = 8;
+  typedef logic [A-1:0] T;
+endpackage
+
+package P2;
+  localparam A = 4;
+  typedef P1::T T;
+endpackage
+
+bit failed = 1'b0;
+
+`define check(expr, val) \
+  if (expr !== val) begin \
+    $display("FAILED: %s, expected %0d, got %0d", `"expr`", val, expr); \
+    failed = 1'b1; \
+  end
+
+module test;
+  localparam A = 2;
+
+  typedef P2::T T;
+
+  T x;
+
+  initial begin
+    x = 8'hff;
+
+    `check(x, 8'hff);
+    `check($bits(x), 8);
+    `check($bits(T), 8);
+    `check($bits(P1::T), 8);
+    `check($bits(P2::T), 8);
+
+    if (!failed) begin
+      $display("PASSED");
+    end
+  end
+
+endmodule
