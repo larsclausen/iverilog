162c1cc124ea0d3272fc6770801cb2df4d9293ce
Lars-Peter Clausen <lars@metafoo.de>
2022-04-17 18:15:07 +0200

    Require SystemVerilog for module port unpacked arrays
    Verilog does not support unpacked arrays for ports, this is only supported
    in SystemVerilog. For task and function ports this is already enforced, but
    for module ports they are accepted.
    
    Add a check to ensure that module unpacked array ports are only allowed in
    SystemVerilog.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- pform.cc
+++ pform.cc
@@ -2615,7 +2615,8 @@ void pform_module_define_port(const struct vlltype&li,
 	    cur->set_data_type(vtype);
 
       if (urange) {
-	    cur->set_unpacked_idx(*urange);
+	    if (pform_requires_sv(li, "Module port with unpacked dimensions"))
+		  cur->set_unpacked_idx(*urange);
       }
 
       pform_bind_attributes(cur->attributes, attr, keep_attr);
