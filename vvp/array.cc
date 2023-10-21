larsclausen/iverilog

commit 0eb01fff1e73609db1f346a4ea1765245686eeb5

vvp: Fix initial value propagation for 2-state non-automatic arrays
For 2-state non-automatic arrays currently real 0.0 value is propagated as
the initial value. This will cause an assert at the downstream receivers
which expect a vector.

Make sure that the vector value 0 is propagated instead for 2-state
vectors.

Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

vvp/array.cc
@@ -1261,11 +1261,15 @@ static void array_attach_port(vvp_array_t array, vvp_fun_arrayport*fun)
       array->ports_ = fun;
       if (!array->get_scope()->is_automatic()) {
               /* propagate initial values for variable arrays */
-            if (array->vals4) {
-                  vvp_vector4_t tmp(array->vals_width, BIT4_X);
+            if (!vpi_array_is_real(array)) {
+                  vvp_bit4_t init;
+                  if (array->vals4)
+                      init = BIT4_X;
+                  else
+                      init = BIT4_0;
+                  vvp_vector4_t tmp(array->vals_width, init);
                   schedule_init_propagate(fun->net_, tmp);
-            }
-            if (array->vals) {
+            } else {
                   schedule_init_propagate(fun->net_, 0.0);
             }
       }

commit d0f3c4db6458a6a8861206952995785e77103543

vvp: Don't schedule initial value propagation for net array ports
An initial value propagation should only be scheduled for variable array
ports, but not for net array port since those do not contain any values.

This got accidentally broken when fixing support for 2-state variable
array ports.

Add a check that only does the initial value propagation if the port is for
a variable array.

Fixes: 0eb01fff1e73 ("vvp: Fix initial value propagation for 2-state non-automatic arrays")
Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

vvp/array.cc
@@ -1259,7 +1259,8 @@ static void array_attach_port(vvp_array_t array, vvp_fun_arrayport*fun)
       assert(fun->next_ == 0);
       fun->next_ = array->ports_;
       array->ports_ = fun;
-      if (!array->get_scope()->is_automatic()) {
+      if (!array->get_scope()->is_automatic() &&
+          (array->vals4 || array->vals)) {
               /* propagate initial values for variable arrays */
             if (!vpi_array_is_real(array)) {
                   vvp_bit4_t init;